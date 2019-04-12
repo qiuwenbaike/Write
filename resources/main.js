d4cfc8172e42b1106b734e4c6b08136d7d3ac102
Petr Kajzar <petr.kajzar@lf1.cuni.cz>
2019-04-12 14:08:43 +0200

    Add proper Bootstrap CSS classes to mw.util.addPortletLink() links

--- resources/main.js
+++ resources/main.js
@@ -1,10 +1,43 @@
 /*! Medik | CC0 license | https://bitbucket.org/wikiskripta/medik */
 
+var Medik = {
+
+  /*
+   * Hamburger menu
+   * opens navigation sidebar and login/user menu
+   */
+  hamburger : function() {
+    $(".mw-hamb").click(function() {
+      $("#mw-navigation nav").toggle("fast");
+      $("#user-tools > button").toggle("fast");
+    });
+  },
+
+  /*
+   * Site navigation
+   * adds proper Bootstrap CSS class to links added via mw.util.addPortletLink()
+   */
+  navigation : function() {
+    $("#p-personal li a:not(.dropdown-item), aside li a:not(.dropdown-item)")
+      .addClass("dropdown-item");
+    $("#mw-navigation li a:not(.nav-link)")
+      .addClass("nav-link");
+  }
+
+}
+
 /*
- * Hamburger menu
- * opens navigation sidebar and login/user menu
+ * Start functions
  */
-$(".mw-hamb").click(function() {
-  $("#mw-navigation nav").toggle("fast");
-  $("#user-tools > button").toggle("fast");
+$(function() {
+  
+  // immediately
+  Medik.hamburger();
+  Medik.navigation();
+
+  // after 10 s
+  setTimeout(function () {
+    Medik.navigation();
+  }, 10000);
+
 });
