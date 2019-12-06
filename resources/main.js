9f2f2d525fac0788b75236ab4b8a3843816b9bc7
Petr Kajzar <petr.kajzar@lf1.cuni.cz>
2019-12-06 10:33:32 +0100

    Add npm to CI/CD

--- resources/main.js
+++ resources/main.js
@@ -1,35 +1,38 @@
-/*! Medik | CC0 license | https://bitbucket.org/wikiskripta/medik */
+/* Medik | CC0 license | https://bitbucket.org/wikiskripta/medik */
+$( function () {
 
-/*
- * Site navigation
- * adds proper Bootstrap CSS class to links added via mw.util.addPortletLink()
- */
-function medik_navigation() {
-    $("#p-personal li a:not(.dropdown-item), aside li a:not(.dropdown-item)")
-      .addClass("dropdown-item");
-    $("#mw-navigation li a:not(.nav-link)")
-      .addClass("nav-link");
-}
+	/*
+	* Site navigation
+	* adds proper Bootstrap CSS class to links added via mw.util.addPortletLink()
+	*/
+	function medikNavigation() {
+		$( '#p-personal li a:not(.dropdown-item), aside li a:not(.dropdown-item)' )
+			.addClass( 'dropdown-item' );
+		$( '#mw-navigation li a:not(.nav-link)' )
+			.addClass( 'nav-link' );
+	}
 
-/*
- * Hamburger menu
- * opens navigation sidebar and login/user menu
- */
-function medik_togglehamb() {
-  $("#mw-navigation nav").toggle("fast");
-}
+	/*
+	* Hamburger menu
+	* opens navigation sidebar and login/user menu
+	*/
+	function medikTogglehamb() {
+		$( '#mw-navigation nav' ).toggle( 'fast' );
+	}
 
-/*
- * Start functions from the wrapper
- */
- 
-// immediately
-medik_navigation();
-$(".mw-hamb").click(medik_togglehamb);
+	/*
+	* Start functions from the wrapper
+	*/
 
-// 10 s after DOM content loaded
-$(function() {
-  setTimeout(function () {
-    medik_navigation();
-  }, 10000);
-});
+	// immediately
+	medikNavigation();
+	$( '.mw-hamb' ).on( 'click', medikTogglehamb );
+
+	// 10 s after DOM content loaded
+	$( function () {
+		setTimeout( function () {
+			medikNavigation();
+		}, 10000 );
+	} );
+
+} );
