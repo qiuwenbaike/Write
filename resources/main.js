59ce77043cf03ad08c1ad967f759ef527e499c13
Petr Kajzar <petr.kajzar@centrum.cz>
2020-09-23 11:41:52 +0200

    Use echo notifications popups on smaller screens
    For screens <= 650 px it's very painful to open the notifications popup,
    it does not display properly and breaks the page. That's why this has
    been disabled and hidden by CSS for widths <= 650 px.
    
    Now I would like to try another approach: let's show the echo
    notifications icons (badges), but disable the onclick event and let
    MediaWiki redirect to Special:Notifications page instead, which is quite
    default link under the hood of the badges.

--- resources/main.js
+++ resources/main.js
@@ -2,9 +2,9 @@
 $( function () {
 
 	/*
-	* Site navigation
-	* adds proper Bootstrap CSS class to links added via mw.util.addPortletLink()
-	*/
+	 * Site navigation
+	 * adds proper Bootstrap CSS class to links added via mw.util.addPortletLink()
+	 */
 	function medikNavigation() {
 		$( '#p-personal li a:not(.dropdown-item), aside li a:not(.dropdown-item)' )
 			.addClass( 'dropdown-item' );
@@ -13,26 +13,40 @@ $( function () {
 	}
 
 	/*
-	* Hamburger menu
-	* opens navigation sidebar and login/user menu
-	*/
+	 * Remove echo notifications popup on smaller screens
+	 */
+	function medikRemoveEchoPopup() {
+		if ( $( window ).width() <= 650 ) {
+			$( 'a.mw-echo-notifications-badge' ).off( 'click' );
+		}
+	}
+
+	/*
+	 * Hamburger menu
+	 * opens navigation sidebar and login/user menu
+	 */
 	function medikTogglehamb() {
 		$( '#mw-navigation nav' ).toggle( 'fast' );
 	}
 
 	/*
-	* Start functions from the wrapper
-	*/
+	 * Start functions from the wrapper
+	 */
 
 	// immediately
 	medikNavigation();
+	medikRemoveEchoPopup();
 	$( '.mw-hamb' ).on( 'click', medikTogglehamb );
 
-	// 10 s after DOM content loaded
-	$( function () {
-		setTimeout( function () {
-			medikNavigation();
-		}, 10000 );
-	} );
+	// repeat every 1 s for 10 s after DOM content loaded
+	window.medikVarI = 0;
+	window.medikTimer = window.setInterval( function () {
+		medikNavigation();
+		medikRemoveEchoPopup();
+		window.medikVarI++;
+		if ( window.medikVarI === 10 ) {
+			window.clearInterval( window.medikTimer );
+		}
+	}, 1000 );
 
 } );
