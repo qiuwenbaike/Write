d104f69d3bcd45240cfd21666a076ab4f67d5cee
WaitSpring <me@waitspring.com>
2025-10-18 21:10:13 +0800

    update SVGO config

--- .svgo.config.js
+++ .svgo.config.js
@@ -1,6 +1,11 @@
 'use strict';
+/**
+ * SVGO Configuration
+ * Compatible to v3.0.0+
+ * Recommended options from:
+ * https://www.mediawiki.org/wiki/Manual:Coding_conventions/SVG#Exemplified_safe_configuration
+ */
 
-// https://www.mediawiki.org/wiki/Manual:Coding_conventions/SVG#Exemplified_safe_configuration
 module.exports = {
 	js2svg: {
 		eol: 'lf',
@@ -13,7 +18,14 @@ module.exports = {
 			name: 'preset-default',
 			params: {
 				overrides: {
-					removeViewBox: false
+					cleanupIds: false,
+					removeDesc: false,
+					// If the SVG doesn't start with an XML declaration, then its MIME type will
+					// be detected as "text/plain" rather than "image/svg+xml" by libmagic and,
+					// consequently, MediaWiki's CSSMin CSS minifier. libmagic's default database
+					// currently requires that SVGs contain an XML declaration:
+					// https://github.com/threatstack/libmagic/blob/master/magic/Magdir/sgml#L5
+					removeXMLProcInst: false
 				}
 			}
 		},
